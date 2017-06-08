c40dd4d927d281bc1a0b45a93cfcbab51e08ff15
Lashaunna Samuels <shaunna191@hotmail.com>
2017-06-08 14:23:21 -0400

    - Adjusted the padding-top of #slide3 - Offseted the navbar scroll by 1px

--- js/main.js
+++ js/main.js
@@ -1,52 +1,37 @@
-// jQuery for sliding nav
- $(document).ready(function() {
-
-      $('a[href*=#]').each(function() {
-        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
-        && location.hostname == this.hostname
-        && this.hash.replace(/#/,'') ) {
-          var $targetId = $(this.hash), $targetAnchor = $('[name=' + this.hash.slice(1) +']');
-          var $target = $targetId.length ? $targetId : $targetAnchor.length ? $targetAnchor : false;
-           if ($target) {
-             var targetOffset = $target.offset().top;
-
-// jQuery click function remove and add class "active" + scroll to the #div
-    $(this).click(function() {
-                $("#nav li a").removeClass("active");
-                $(this).addClass('active');
-               $('html, body').animate({scrollTop: targetOffset}, 500);
-               return false;
-             });
-          }
-        }
-  });
-
-  // change navbar colour
+$(document).ready(function () { 
+  $('a[href*=#]').each(function () {  
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')   && location.hostname == this.hostname   && this.hash.replace(/#/, '')) {   
+      var $targetId = $(this.hash),
+        $targetAnchor = $('[name=' + this.hash.slice(1) + ']');   
+      var $target = $targetId.length ? $targetId : $targetAnchor.length ? $targetAnchor : !1;    
+      if ($target) {     
+        var targetOffset = $target.offset().top - 1;
+        $(this).click(function () {
+          $("#nav li a").removeClass("active");
+          $(this).addClass('active');      
+          $('html, body').animate({
+            scrollTop: targetOffset
+          }, 500);      
+          return !1;     
+        });   
+      }  
+    } 
+  });
   var a = $(".container-fluid").offset();
+  $(document).scroll(function () {
+    if ($(this).scrollTop() > 780) {
+      $('.navbar-default').addClass('after-scroll-nav-border')
+    } else {
+      $('.navbar-default').removeClass('after-scroll-nav-border')
+    }
+  });
+  $(document).onscroll = navbarScroll;
 
-    $(document).scroll(function(){
-      if($(this).scrollTop() > 780)
-      {
-       $('.navbar-default').addClass('after-scroll-nav-border');
-       
-      }
-      else
-      {
-        $('.navbar-default').removeClass('after-scroll-nav-border');
-       
-      }
-    });
-
-    // affect the navbar fade when scrolling between slides
-    $(document).onscroll = navbarScroll;
-    function navbarScroll () {
-      if ($(this).scrollTop() > 1005) {
-          $('.navbar-default').fadeIn();
-      }
-      else {
-          $('.navbar-default').fadeOut();
-      }
-    }; 
-  
-
-    });
+  function navbarScroll() {
+    if ($(this).scrollTop() > 1005) {
+      $('.navbar-default').fadeIn()
+    } else {
+      $('.navbar-default').fadeOut()
+    }
+  }
+})
